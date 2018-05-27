ec6c9d9c740b77c761ff133b8af4c983602ca27f
oshou <adf1985adf@gmail.com>
2018-05-27 12:27:34 +0900

    modify Single pattern

--- php/Singleton/SingletonSample.class.php
+++ php/Singleton/SingletonSample.class.php
@@ -1,51 +1,62 @@
 <?php
 
+/**
+ * http://d.hatena.ne.jp/shimooka/20141212/1418363981
+ *
+ * @author test
+ */
 class SingletonSample
 {
-    /**
-     * メンバ変数
-     */
     private $id;
 
-    /*+
-     * 唯一のインスタンスを保持する変数
+    /**
+     * 唯一のインスタンスを保持
      */
     private static $instance;
 
+
     /**
      * コンストラクタ
-     * IDとして、生成日時のハッシュ値を作成
+     * IDとして生成日時のハッシュ値を作成
      */
     private function __construct()
     {
         $this->id = md5(date('r').mt_rand());
     }
 
+
     /**
      * 唯一のインスタンスを返すためのメソッド
+     *
+     * @return SingletonSampleインスタンス
      */
     public static function getInstance()
     {
-        if (!isset(self::$instance)){
+        if (!isset(self::$instance)) {
             self::$instance = new SingletonSample();
-            echo "a SingletonSample instance was created!\n";
+            echo 'a SingletonSample instance was created!';
         }
         return self::$instance;
     }
 
+
     /**
      * IDを返す
+     *
+     * @return インスタンスのID
      */
     public function getID()
     {
         return $this->id;
     }
 
+
     /**
      * インスタンス複製を許可しない
      * @throws RuntimeException
      */
-    public final function __clone() {
-        throw new RuntimeException ('Clone is not allowed against '.get_class($this));
+    final public function __clone()
+    {
+        throw new RuntimeException('Clone is not allowed against '.get_class($this));
     }
 }
--- php/Singleton/singleton_client.class.php
+++ php/Singleton/singleton_client.class.php
@@ -9,19 +9,20 @@ $instance1 = SingletonSample::getInstance();
 sleep(1);
 $instance2 = SingletonSample::getInstance();
 
-echo "<hr/>";
+echo '<hr>';
 
 /**
  * 2つのインスタンスIDが同一か確認する
  */
-echo 'instanceID: '.$instance1->getID()."<br/>\n";
-echo 'instance1->getID() === $instance2->getID(): '.($instance1->getID() === $instance2->getID() ? 'true' : 'false')."\n";
+echo 'instance1-ID: '.$instance1->getID()."<br>";
+echo 'instance2-ID: '.$instance2->getID()."<br>";
+echo 'instance1->getID() === $instance2->getID(): '.($instance1->getID() === $instance2->getID() ? 'true' : 'false')."<br>";
 
 /**
  * 2つのインスタンスが同一か確認する
  */
-echo 'instance1 === instance2: '.($instance1 === $instance2 ? 'true' : 'false')."\n";
-echo "<hr/>";
+echo 'instance1 === instance2: '.($instance1 === $instance2 ? 'true' : 'false').PHP_EOL;
+echo '<hr>';
 
 /**
  * 複製出来ない事を確認する
